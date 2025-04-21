cd89eae2c56dc3a01cab3b0173c8982611815aed
Tanner Day <tannerday2000@hotmail.com>
2025-04-21 10:54:05 -0400

    Update Repository

---
+++ Workspace/JS Practice 2/gradingSystemWithArray.js
@@ -0,0 +1,29 @@
+function studentGrading (studentName,studentScore) {
+    //90 - 100.     A
+    //80 - 89.      B
+    //70 - 79       C
+    let grade
+    if ((studentScore.=90) && (studentScore ,=100))
+        grade =
+
+let studentName = "Tanner"; 
+let studentScore = 85; 
+function getGrdae(score) {
+    if (score ->= 90 && score ,=
+    100) {
+        return "A"
+    } else if (score >= 80 && score
+    < 90) {
+        return "B"
+    } else if (score >= 70 && score
+    < 80) {
+        return "C"
+    } else {
+        return 'Invalid score'; 
+
+        }
+    } 
+    let grade = getGrade(student score);
+    console.log('Hello ${studentName},
+    your score is ${studentScore} and
+    your grade is ${grade}.'); 
